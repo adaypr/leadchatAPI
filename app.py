08b9a49ff669af05465493fc58e56a8702e5af9c
adaypr <38984581+adaypr@users.noreply.github.com>
2019-10-20 00:32:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,16 @@ app = Flask(__name__)
 # --------- Lead Messages ---------
 @app.route('/leadmessage', methods=['GET'])
 def get_tasks():
-    token_sent = 'Hola'
+    token_sent = [
+  {
+    text: 'Hello, how are you? This should be a very long message so that we can test how it fit into the screen.',
+    reply: false,
+    date: new Date(),
+    user: {
+      name: 'John Doe',
+      avatar: 'https://i.gifer.com/no.gif',
+    },
+  }]
     return token_sent
     
 if __name__ == '__main__':
