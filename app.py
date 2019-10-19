fd37687b75cd1f3d640df196ea325b05f1c8f681
adaypr <38984581+adaypr@users.noreply.github.com>
2019-10-20 01:15:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #!flask/bin/python
 from flask import Flask, jsonify, request, json, Response
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 # --------- Lead Messages ---------
@@ -9,7 +11,7 @@ app = Flask(__name__)
 def get_tasks():
     
     x =  '{ "name":"John", "age":30, "city":"New York"}'    
-    return Response(x, mimetype='text/plain') 
+    return Response(json.loads(x), mimetype='application/json') 
     
 if __name__ == '__main__':
     app.run(debug=True)
