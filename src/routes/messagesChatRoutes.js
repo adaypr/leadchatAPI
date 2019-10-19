a860c747d3ba4d873a3c70c1099e6a8b4293c58d
adaypr <38984581+adaypr@users.noreply.github.com>
2019-10-19 21:32:16 +0200

    Update messagesChatRoutes.js

--- src/routes/messagesChatRoutes.js
+++ src/routes/messagesChatRoutes.js
@@ -1,35 +1,36 @@
-//const QuoteService = require('../models/QuoteServiceModel');
 var express_logger = require('express-logger-unique-req-id');
 let logger = express_logger.getLogger();
 const uuid = require('node-uuid');
 
 module.exports = function (app) {
-  app.get('/messagesChat', async function(req, res) {
+  app.post('/mSIPQuotation', async function(req, res) {
 	try{
 		const OrderData = {
 		  Service_Name : req.body.service,
-		};		
+		};
+		console.log(req.body);
+		//console.log('IP: ' + req.connection.remoteAddress)
+		console.log('IPs: ' + req.ip);
+		if(req.body.service=='StandAlone'){			
 			res.json({
 				  "data": [
-				    {
-				    "text": "Hello, how are you? This should be a very long message so that we can test how it fit into the screen.",
-				    "reply": false,
-				    "date": new Date(),
-				    "user": {
-				      "name": "John Doe",
-				      "avatar": "https://i.gifer.com/no.gif",
-				    },
-				  },
-				  {
-				    "text": "Hello, how are you? This should be a very long message so that we can test how it fit into the screen.",
-				    "reply": true,
-				    "date": new Date(),
-				    "user": {
-				      "name": "Aday Perera",
-				      "avatar": "https://i.gifer.com/no.gif",
-				    }  
+					{
+					    "text": 'Hello, how are you? This should be a very long message so that we can test how it fit into the screen.',
+					    "reply": false,
+					    "date": new Date(),
+					    "user": {
+					      "name": 'John Doe',
+					      "avatar": 'https://i.gifer.com/no.gif',
+					    }
+					  }
 				  ]
-				});											
+				});	
+		}		
+		else{	
+			res.json({
+				"Error": '201 ERROR'
+				});
+		}
 	}catch (e){
 		res.json({
 			"Error Catch": '201 ERROR'
@@ -37,4 +38,3 @@ module.exports = function (app) {
 	}
   });
 }
-
