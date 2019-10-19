f62662f3f137544a435966219d19f723cf10b418
adaypr <38984581+adaypr@users.noreply.github.com>
2019-10-20 00:38:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,8 @@ app = Flask(__name__)
 @app.route('/leadmessage', methods=['GET'])
 def get_tasks():
     
-    token_sent = '[{"text": "Hello, how are you? This should be a very long message so that we can test how it fit into the screen.",
-    "reply": "false",
-    "date": "20/10/2019",
-    "user": {
-      "name": 'John Doe',
-      "avatar": 'https://i.gifer.com/no.gif',
-    },
-  }]'
-    return json.loads(token_sent)
+    x =  '{ "name":"John", "age":30, "city":"New York"}'
+    return son.loads(x)
     
 if __name__ == '__main__':
     app.run(debug=True)
