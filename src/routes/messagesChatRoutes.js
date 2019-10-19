60275dd20b77b03e02bf7e39760ba0368cb66927
adaypr <38984581+adaypr@users.noreply.github.com>
2019-10-19 21:16:09 +0200

    Update and rename QuotationMockUpRoutes.js to messagesChatRoutes.js

---
+++ src/routes/messagesChatRoutes.js
@@ -0,0 +1,39 @@
+//const QuoteService = require('../models/QuoteServiceModel');
+var express_logger = require('express-logger-unique-req-id');
+let logger = express_logger.getLogger();
+const uuid = require('node-uuid');
+
+module.exports = function (app) {
+  app.get('/messagesChat', async function(req, res) {
+	try{
+		const OrderData = {
+		  Service_Name : req.body.service,
+		};		
+			res.json([
+				  {
+				    text: 'Hello, how are you? This should be a very long message so that we can test how it fit into the screen.',
+				    reply: false,
+				    date: new Date(),
+				    user: {
+				      name: 'John Doe',
+				      avatar: 'https://i.gifer.com/no.gif',
+				    },
+				  },
+				  {
+				    text: 'Hello, how are you? This should be a very long message so that we can test how it fit into the screen.',
+				    reply: true,
+				    date: new Date(),
+				    user: {
+				      name: 'Aday Perera',
+				      avatar: 'https://i.gifer.com/no.gif',
+				    }  
+				]);	
+		
+	}catch (e){
+		res.json({
+			"Error Catch": '201 ERROR'
+			});
+	}
+  });
+}
+
