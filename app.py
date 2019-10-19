39552015c61738f08f25e923fbb35b578dc304ce
adaypr <38984581+adaypr@users.noreply.github.com>
2019-10-20 01:23:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,20 @@ CORS(app)
 @app.route('/leadmessage', methods=['GET'])
 def get_tasks():
     
-    x =  '{ "name":"John", "age":30, "city":"New York"}'    
+    x =  '{ "name":"John", "age":30, "city":"New York"}'   
+    
+    x = '{ "data": [
+  {
+    "text": "Hello, how are you? This should be a very long message so that we can test how it fit into the screen.",
+    "reply": "false",
+    "date": "20/10/2019",
+    "user": {
+      "name": "John Doe",
+      "avatar": "https://i.gifer.com/no.gif",
+    }
+      ]}'
+    
+    
     return json.loads(x) 
     
 if __name__ == '__main__':
