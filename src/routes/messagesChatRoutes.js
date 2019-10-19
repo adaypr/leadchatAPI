42183c07820483de893616bb371f58dbc3f7a39e
adaypr <38984581+adaypr@users.noreply.github.com>
2019-10-19 21:26:58 +0200

    Update messagesChatRoutes.js

--- src/routes/messagesChatRoutes.js
+++ src/routes/messagesChatRoutes.js
@@ -9,23 +9,24 @@ module.exports = function (app) {
 		const OrderData = {
 		  Service_Name : req.body.service,
 		};		
-			res.json({[
+			res.json({
+				"data": [
 				  {
-				    text: 'Hello, how are you? This should be a very long message so that we can test how it fit into the screen.',
-				    reply: false,
-				    date: new Date(),
-				    user: {
-				      name: 'John Doe',
-				      avatar: 'https://i.gifer.com/no.gif',
+				    "text": "Hello, how are you? This should be a very long message so that we can test how it fit into the screen.",
+				    "reply": false,
+				    "date": new Date(),
+				    "user": {
+				      "name": "John Doe",
+				      "avatar": "https://i.gifer.com/no.gif",
 				    },
 				  },
 				  {
-				    text: 'Hello, how are you? This should be a very long message so that we can test how it fit into the screen.',
-				    reply: true,
-				    date: new Date(),
-				    user: {
-				      name: 'Aday Perera',
-				      avatar: 'https://i.gifer.com/no.gif',
+				    "text": "Hello, how are you? This should be a very long message so that we can test how it fit into the screen.",
+				    "reply": true,
+				    "date": new Date(),
+				    "user": {
+				      "name": "Aday Perera",
+				      "avatar": "https://i.gifer.com/no.gif",
 				    }  
 				]});	
 		
