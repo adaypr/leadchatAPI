b613c3f569aee2b8359064cb53f3411f0017d106
adaypr <38984581+adaypr@users.noreply.github.com>
2019-10-21 18:27:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CORS(app)
 def get_tasks():
     
     #x =  '{ "name":"John", "age":30, "city":"New York"}'
-    x = '{  "data": [    {      "text": "Yeahhh Aday. Its works!",      "reply": false,      "date": "2019-10-19T19:57:45.465Z",      "user": {        "name": "Aday Perera",        "avatar": "https://www.socialtools.me/blog/wp-content/uploads/2016/04/foto-de-perfil.jpg"      }    },     {      "text": "Mi respuesta",      "reply": true,      "date": "2019-10-19T20:57:45.465Z",      "user": {        "name": "Aday Perera",        "avatar": "https://www.socialtools.me/blog/wp-content/uploads/2016/04/foto-de-perfil.jpg"      }    }  ]}'
+    x = '{  "data": [  {    "text": "Hello, how are you? This should be a very long message so that we can test how it fit into the screen.",    "reply": false,    "date": "2019-10-19T20:57:45.465Z",    "user": {      "name": "John Doe",      "avatar": "https://i.gifer.com/no.gif",    },  },  {    "text": "Hello, how are you? This should be a very long message so that we can test how it fit into the screen.",    "reply": true,    "date": "2019-10-19T20:57:45.465Z",    "user": {      "name": "Aday Perera",      "avatar": "https://i.gifer.com/no.gif",    },  }]}'
     return json.loads(x) 
     
 if __name__ == '__main__':
