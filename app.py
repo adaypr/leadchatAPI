ebe33c17c455b566ebfdd0971453cdf8079edd4c
adaypr <38984581+adaypr@users.noreply.github.com>
2019-10-21 18:09:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CORS(app)
 def get_tasks():
     
     #x =  '{ "name":"John", "age":30, "city":"New York"}'
-    x = '{  "data": [    {      "text": "Yeahhh Aday. Its works!",      "reply": false,      "date": "2019-10-19T19:57:45.465Z",      "user": {        "name": "Aday Perera",        "avatar": "https://www.socialtools.me/blog/wp-content/uploads/2016/04/foto-de-perfil.jpg"      }    }  ]}'
+    x = '{"data": [{"text": "Yeahhh Aday. Its works!","reply": false,"date": "2019-10-19T19:57:45.465Z","user": {"name": "Aday Perera","avatar": "https://www.socialtools.me/blog/wp-content/uploads/2016/04/foto-de-perfil.jpg"}}, {"text": "Mi respuesta","reply": true,"date": "2019-10-19T20:57:45.465Z","user": {"name": "Aday Perera","avatar": "hhttps://www.windsurf.co.uk/wp-content/uploads/2019/04/5W9A1337-631x420.jpg"}}]}'
     return json.loads(x) 
     
 if __name__ == '__main__':
