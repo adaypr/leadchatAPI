dda98caa2880ffc0a3e49229a2bba73648d6a0b3
adaypr <38984581+adaypr@users.noreply.github.com>
2019-10-21 18:14:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CORS(app)
 def get_tasks():
     
     #x =  '{ "name":"John", "age":30, "city":"New York"}'
-    x = '{"data": [{"text": "Yeahhh Aday. Its works!","reply": false,"date": "2019-10-19T19:57:45.465Z","user": {"name": "Aday Perera","avatar": "https://www.socialtools.me/blog/wp-content/uploads/2016/04/foto-de-perfil.jpg"}}, {"text": "Mi respuesta","reply": true,"date": "2019-10-19T20:57:45.465Z","user": {"name": "Kai Lenny","avatar": "https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/24796315_10155998989794312_1964191819925532910_n.jpg?_nc_cat=1&_nc_oc=AQnVRfzkGehB1S98rKdVTMCl5uTXqlrPTSmwTRv3TXruJC9uQKdgTtQvoNzjoK5GeNs&_nc_ht=scontent-lht6-1.xx&oh=e82c572c8920bb26b1fda8f6725b10af&oe=5E28B586"}}]}'
+    x = '{"data": [{"text": "Yeahhh Aday. Its works!","reply": false,"date": "2019-10-19T19:57:45.465Z","user": {"name": "Aday Perera","avatar": "https://www.socialtools.me/blog/wp-content/uploads/2016/04/foto-de-perfil.jpg"}}, {"text": "Mi respuesta","reply": true,"date": "2019-10-19T20:57:45.465Z","user": {"name": "Kai Lenny","avatar": "https://www.windsurf.co.uk/wp-content/uploads/2019/04/5W9A1337-631x420.jpg"}}]}'
     return json.loads(x) 
     
 if __name__ == '__main__':
